70fb62a019cf2db838a73acd4e0e41f73867f3c1
Danny Niedzwiedzki <danielniedzwiedzki.1@gmail.com>
2021-11-10 19:55:11 -0500

    connected backed

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express"); // express is for routing
+const app = express(); //creates the "app" that routes
+
+const bodyParser = require('body-parser');
+
+
+const PORT = process.env.port || 3000
+
+let list = ['apple', 'banana', 'orange']
+
+app.get('/', (req,res)=>{
+  res.send(list)
+})
+
+
+
+
+
+
+app.listen(PORT, ()=> console.log( `server running ${PORT}`))
